1b3b7e8bab1d655d72b21a3f51612d0c3759b877
hoonjong <qkr7627@naver.com>
2021-05-24 23:32:11 +0900

    2021.05.24 study-01

---
+++ src/firstday.java
@@ -0,0 +1,12 @@
+public class firstday {
+ public static void main(String[] args) {
+     int a = 3;
+     double doubledata = 1.249534932;
+     float flodata = 232323.3222f;
+     int b = 5;
+     System.out.printf("%d + %d = %d\n", 3, 5, 8);
+     System.out.println("안녕" + "하세요");
+     System.out.println(3+5);
+     System.out.println(a);
+ }
+}
---
+++ src/studyEx1.java
@@ -0,0 +1,8 @@
+public class studyEx1 {
+    public static void main(String[] args) {
+        System.out.printf("저의 나이는 %d 입니다. %n" , 26);
+        System.out.printf("당신의 나이는 %d 입니까??? %n" , 29);
+    }
+
+
+}
