cde96950cec1ed0093d33c43512672d1fee59d84
hoonjong <qkr7627@naver.com>
2021-06-01 13:20:19 +0900

    Study

--- src/studyEx1.java
+++ src/studyEx1.java
@@ -2,6 +2,15 @@ public class studyEx1 {
     public static void main(String[] args) {
         System.out.printf("저의 나이는 %d 입니다. %n" , 26);
         System.out.printf("당신의 나이는 %d 입니까??? %n" , 29);
+        System.out.println("우리는 " + " 착하다 ? ");
+        System.out.printf("당신은 사과 %d 개를 갖고 계시나요?\n", 10);
+
+        int[] intarray = {1, 2, 3, 4, 5, 6, 7};
+
+
+
+
+
     }
 
 
